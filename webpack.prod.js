95b2eeeade00d0a4a43bccca1db21aae266696f9
Sung Kim <sungmkim80@gmail.com>
2019-01-11 22:29:21 -0500

    Separated dev & production webpack configs

---
+++ webpack.prod.js
@@ -0,0 +1,32 @@
+// Created using
+// 1. https://webpack.jakoblind.no/
+// 2. https://webpack.js.org/guides/typescript/
+// const webpack = require("webpack");
+const path = require("path");
+
+const config = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(ts|tsx)?$/,
+        loader: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.(js|jsx)$/,
+        use: "babel-loader",
+        exclude: /node_modules/
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js", ".jsx"]
+  }
+};
+
+module.exports = config;
