c5f022a154f55683e1c28f69b1e6af4febb7667e
Sung Kim <sungmkim80@gmail.com>
2019-01-17 23:23:29 -0500

    🥊 Removed unused commented out code in rollup config & fixed author name

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,7 @@
 import resolve from "rollup-plugin-node-resolve";
 import typescript from "rollup-plugin-typescript2";
-// import uglify from "rollup-plugin-uglify";
-// const { uglify } = require("rollup-plugin-uglify");
-// import { uglify } from "rollup-plugin-uglify";
-// // https://github.com/TrySound/rollup-plugin-uglify/issues/37#issuecomment-329108094
-// import { minify } from "uglify-es";
-// const uglify = require("rollup-plugin-uglify");
 import commonjs from "rollup-plugin-commonjs";
 import { terser } from "rollup-plugin-terser";
-// import uglify from "rollup-plugin-uglify-es";
 import analyze from "rollup-plugin-analyzer";
 
 // https://hackernoon.com/building-and-publishing-a-module-with-typescript-and-rollup-js-faa778c85396
@@ -30,7 +23,6 @@ export default {
   ],
   // Optimization: Don't include react in the library.
   // https://hackernoon.com/making-of-a-component-library-for-react-e6421ea4e6c7
-  // external: ["react", "react-dom"],
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {})
