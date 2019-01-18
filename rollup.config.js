e75febf32689e62159c5fa25a3015b0fa7cb6e94
Sung Kim <sungmkim80@gmail.com>
2019-01-17 21:10:51 -0500

    Exports typescript definition files

--- rollup.config.js
+++ rollup.config.js
@@ -1,33 +1,44 @@
-import resolve from "rollup-plugin-node-resolve";
-// https://github.com/rollup/rollup-plugin-typescript
-import typescript from "rollup-plugin-typescript";
-import commonjs from "rollup-plugin-commonjs";
-import { terser } from "rollup-plugin-terser";
-import analyze from "rollup-plugin-analyzer";
+// import resolve from "rollup-plugin-node-resolve";
+import typescript from "rollup-plugin-typescript2";
+// import commonjs from "rollup-plugin-commonjs";
+// import { terser } from "rollup-plugin-terser";
+// import analyze from "rollup-plugin-analyzer";
+
+// https://hackernoon.com/building-and-publishing-a-module-with-typescript-and-rollup-js-faa778c85396
+import pkg from "./package.json";
 
 export default {
-  // Optimization: Don't include react in the library.
-  // https://hackernoon.com/making-of-a-component-library-for-react-e6421ea4e6c7
-  external: ["react", "react-dom"],
   input: "./src/dangerous.tsx",
   output: [
     {
-      file: "dist/dangerous.cjs.js",
+      file: pkg.main,
       format: "cjs",
-      name: "dangerous"
+      name: pkg.name
     },
     {
-      file: "dist/dangerous.esm.js",
+      file: pkg.module,
       format: "es",
-      name: "dangerous"
+      name: pkg.name
     }
   ],
+  // Optimization: Don't include react in the library.
+  // https://hackernoon.com/making-of-a-component-library-for-react-e6421ea4e6c7
+  // external: ["react", "react-dom"],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {})
+  ],
   plugins: [
-    resolve(),
-    typescript(),
-    // the ".ts" extension is required
-    commonjs({ extensions: [".js", ".ts", , ".tsx"] }),
-    terser(),
-    analyze()
+    typescript({
+      typescript: require("typescript")
+    })
   ]
 };
+// plugins: [
+//   resolve(),
+//   typescript({ declaration: true }),
+//   // // the ".ts" extension is required
+//   // commonjs({ extensions: [".js", ".ts", , ".tsx"] }),
+//   // terser(),
+//   analyze()
+// ]
